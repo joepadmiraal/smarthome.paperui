0bf92196e4316a1e33e49ff4bc52f914089d46d5
Dennis Nobel <dennis@dnobel.de>
2015-07-21 13:47:21 +0200

    Supported new removing process in Paper UI.
    Signed-off-by: Dennis Nobel <dennis@dnobel.de>

--- extensions/ui/org.eclipse.smarthome.ui.paperui/web/js/controllers.configuration.js
+++ extensions/ui/org.eclipse.smarthome.ui.paperui/web/js/controllers.configuration.js
@@ -107,21 +107,17 @@ angular.module('SmartHomeManagerApp.controllers.configuration',
 		thingRepository.getAll(true);	
 	}
 	$scope.remove = function(thing, event) {
-		var confirm = $mdDialog.confirm()
-	      .title('Remove ' + (thing.item ? thing.item.label : thing.UID))
-	      .content('Would you like to remove the thing from the system?')
-	      .ariaLabel('Remove Thing')
-	      .ok('Remove')
-	      .cancel('Cancel')
-	      .targetEvent(event);
-	    $mdDialog.show(confirm).then(function() {
-	    	thingSetupService.remove({thingUID: thing.UID}, function() {
-                $scope.refresh();
-                toastService.showDefaultToast('Thing removed');
-            });
-	    });
-	    event.stopImmediatePropagation();
-	};
+        $mdDialog.show({
+            controller : 'RemoveThingDialogController',
+            templateUrl : 'partials/dialog.removething.html',
+            targetEvent : event,
+            hasBackdrop: true,
+            locals: {thing: thing}
+        }).then(function() {
+            $scope.refresh();
+        });
+        event.stopImmediatePropagation();
+    }
 	$scope.refresh();
 }).controller('ViewThingController', function($scope, $mdDialog, toastService, thingTypeRepository, 
 		thingRepository, thingSetupService, homeGroupRepository) {
@@ -141,22 +137,17 @@ angular.module('SmartHomeManagerApp.controllers.configuration',
 		});
 	};
 	$scope.remove = function(thing, event) {
-		var confirm = $mdDialog.confirm()
-	      .title('Remove ' + (thing.item ? thing.item.label : thing.UID))
-	      .content('Would you like to remove the thing from the system?')
-	      .ariaLabel('Remove Thing')
-	      .ok('Remove')
-	      .cancel('Cancel')
-	      .targetEvent(event);
-	    $mdDialog.show(confirm).then(function() {
-	    	thingSetupService.remove({thingUID: thing.UID}, function() {
-	    	    thingRepository.remove(thing);
-	    	    homeGroupRepository.setDirty(true);
-	    	    toastService.showDefaultToast('Thing removed');
-	            $scope.navigateTo('things');
-            });
-	    });
-	};
+        $mdDialog.show({
+            controller : 'RemoveThingDialogController',
+            templateUrl : 'partials/dialog.removething.html',
+            targetEvent : event,
+            hasBackdrop: true,
+            locals: {thing: thing}
+        }).then(function() {
+            $scope.navigateTo('things');
+        });
+        event.stopImmediatePropagation();
+    }
 	
 	$scope.enableChannel = function(thingUID, channelID) {
 		thingSetupService.enableChannel({channelUID: thingUID + ':' + channelID}, function() {
@@ -211,6 +202,23 @@ angular.module('SmartHomeManagerApp.controllers.configuration',
 		$scope.thingType = thingType;
 		$scope.setHeaderText(thingType.description);
 	});
+}).controller('RemoveThingDialogController', function($scope, $mdDialog, toastService, 
+        thingSetupService, homeGroupRepository, thing) {
+    $scope.thing = thing;
+    $scope.close = function() {
+        $mdDialog.cancel();
+    }
+    $scope.remove  = function(thingUID, forceRemove) {    
+        thingSetupService.remove({thingUID: thing.UID, force: forceRemove}, function() {
+            homeGroupRepository.setDirty(true);
+            if(forceRemove) {
+                toastService.showDefaultToast('Thing removed (forced).');
+            } else {
+                toastService.showDefaultToast('Thing removal initiated.');
+            }
+            $mdDialog.hide();
+        });
+    }
 }).controller('EditThingController', function($scope, $mdDialog, toastService, 
 		thingTypeRepository, thingRepository, thingSetupService, homeGroupRepository, configService) {
 	
